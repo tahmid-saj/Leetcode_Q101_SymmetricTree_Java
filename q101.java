cb19f4c15bfe5c30fefcb6a7ec12fef0d7dc482b
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-05-20 17:19:00 -0400

    Create q101.java

---
+++ q101.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (helper(root.left, root.right) == false) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean helper(TreeNode l, TreeNode r) {
+        if (l != null && r != null) {
+            if (helper(l.left, r.right) == false) {
+                return false;
+            }
+
+            if (l.val != r.val) {
+                return false;
+            }
+
+            if (helper(l.right, r.left) == false) {
+                return false;
+            }
+        }
+
+        if ((l == null && r != null) || (l != null && r == null)) {
+            return false;
+        }
+
+        return true;
+    }
+}
